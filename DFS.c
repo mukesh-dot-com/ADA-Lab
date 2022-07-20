c0107eab7ceec0eb580b412c98c629aa895be7db
mukesh-dot-com <96436041+mukesh-dot-com@users.noreply.github.com>
2022-07-20 20:40:13 +0530

    Update DFS.c

--- DFS.c
+++ DFS.c
@@ -1,30 +1,111 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-int adj[10][10],n,visited[10];
-void dfs(int i){
-    int j;
-    printf("\n%d",i);
-    visited[i]=1;
-    for(j=0;j<n;j++){
-        if(!visited[j] && adj[i][j]==1)
-                dfs(j);
-    }
-}
+#include<stdio.h> 
 
-void main(){
-    clock_t start,end;
-    int i,j;
-    printf("Enter the number of nodes\n");
-    scanf("%d",&n);
-    printf("Enter the adjacency matrix\n");
-    for(i=0;i<n;i++)
-        for(j=0;j<n;j++)
-            scanf("%d",&adj[i][j]);
-    for(i=0;i<n;i++)
-            visited[i]=0;
-    start = clock();
-    dfs(0);
-    end = clock();
-    printf("\n%lf",((double) (end - start)) / CLOCKS_PER_SEC);
+#include<conio.h> 
+
+int a[10][10],n,vis[10]; 
+
+int dfs(int); 
+
+void main() 
+
+{ 
+
+ int i,j,src,ans; 
+
+
+
+ for(j=1;j<=n;j++) 
+
+ { 
+
+  vis[j]=0; 
+
+ } 
+
+ printf("\nenter the no of nodes:\t"); 
+
+ scanf("%d",&n); 
+
+ printf("\nenter the adjacency matrix:\n"); 
+
+ for(i=1;i<=n;i++) 
+
+ { 
+
+  for(j=1;j<=n;j++) 
+
+  { 
+
+   scanf("%d",&a[i][j]); 
+
+  } 
+
+ } 
+
+ printf("\nenter the source node:\t"); 
+
+ scanf("%d",&src); 
+
+ ans=dfs(src); 
+
+ if(ans==1) 
+
+ { 
+
+  printf("\ngraph is connected\n"); 
+
+ } 
+
+ else 
+
+ { 
+
+  printf("\ngragh is not connected\n"); 
+
+ } 
+
+ getch(); 
+
+} 
+
+ 
+
+int dfs(int src) 
+
+{ 
+
+ int j; 
+
+ vis[src]=1; 
+
+ for(j=1;j<=n;j++) 
+
+ { 
+
+  if(a[src][j]==1&&vis[j]!=1) 
+
+  { 
+
+   dfs(j); 
+
+  } 
+
+ } 
+
+ for(j=1;j<=n;j++) 
+
+ { 
+
+  if(vis[j]!=1) 
+
+  { 
+
+   return 0; 
+
+  } 
+
+ } 
+
+ return 1; 
+
 }
